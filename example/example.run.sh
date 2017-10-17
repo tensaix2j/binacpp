19c8679a7de28663b37666c0b32125e6a451d429
JJ <JJ>
2017-10-17 17:32:03 +0800

    modified:   example/example 	modified:   example/example.run.sh

--- example/example.run.sh
+++ example/example.run.sh
@@ -9,7 +9,7 @@ libwebsockets_lib=${libwebsockets_dir}/lib
 libbinacpp_dir=../lib/libbinacpp
 libbinacpp_lib=${libbinacpp_dir}/lib
 
-
+export SSL_CERT_FILE=`pwd`/cacert.pem
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$libcurl_lib:$libwebsockets_lib:$libbinacpp_lib
 
 ./example $@
